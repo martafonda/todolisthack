65a1f7d0c1d46d1af6864746d0f5fbf671818d36
Marta Fonda <martafondapascual@gmail.com>
2014-01-04 13:06:18 +0100

    refactor login issues

--- controllers/SignUpHandler.py
+++
@@ -1,8 +0,0 @@
-import webapp2
-from google.appengine.ext.webapp import template
-from google.appengine.api import users
-
-class SignUpHandler(webapp2.RequestHandler):
-    def get(self):
-      values = {'login_url': users.create_login_url("/profile")}
-      self.response.out.write(template.render('views/sign_up.html', values))
